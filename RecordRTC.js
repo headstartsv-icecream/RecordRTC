a20051da0bce794104e623d4464c73cf76b2fd64
곽태욱 <gwak2837@kakao.com>
2021-01-11 22:58:26 +0900

    Remove redefine global variable

--- RecordRTC.js
+++ RecordRTC.js
@@ -1677,100 +1677,100 @@ if (typeof RecordRTC !== "undefined") {
 var browserFakeUserAgent =
   "Fake/5.0 (FakeOS) AppleWebKit/123 (KHTML, like Gecko) Fake/12.3.4567.89 Fake/123.45";
 
-(function (that) {
-  if (!that) {
-    return;
-  }
+// (function (that) {
+//   if (!that) {
+//     return;
+//   }
 
-  if (typeof window !== "undefined") {
-    return;
-  }
+//   if (typeof window !== "undefined") {
+//     return;
+//   }
 
-  if (typeof global === "undefined") {
-    return;
-  }
+//   if (typeof global === "undefined") {
+//     return;
+//   }
 
-  global.navigator = {
-    userAgent: browserFakeUserAgent,
-    getUserMedia: function () {},
-  };
+//   global.navigator = {
+//     userAgent: browserFakeUserAgent,
+//     getUserMedia: function () {},
+//   };
 
-  if (!global.console) {
-    global.console = {};
-  }
+//   if (!global.console) {
+//     global.console = {};
+//   }
 
-  if (
-    typeof global.console.log === "undefined" ||
-    typeof global.console.error === "undefined"
-  ) {
-    global.console.error = global.console.log =
-      global.console.log ||
-      function () {
-        console.log(arguments);
-      };
-  }
+//   if (
+//     typeof global.console.log === "undefined" ||
+//     typeof global.console.error === "undefined"
+//   ) {
+//     global.console.error = global.console.log =
+//       global.console.log ||
+//       function () {
+//         console.log(arguments);
+//       };
+//   }
 
-  // if (typeof document === "undefined") {
-  //   /*global document:true */
-  //   that.document = {
-  //     documentElement: {
-  //       appendChild: function () {
-  //         return "";
-  //       },
-  //     },
-  //   };
+// if (typeof document === "undefined") {
+//   /*global document:true */
+//   that.document = {
+//     documentElement: {
+//       appendChild: function () {
+//         return "";
+//       },
+//     },
+//   };
 
-  //   document.createElement = document.captureStream = document.mozCaptureStream = function () {
-  //     var obj = {
-  //       getContext: function () {
-  //         return obj;
-  //       },
-  //       play: function () {},
-  //       pause: function () {},
-  //       drawImage: function () {},
-  //       toDataURL: function () {
-  //         return "";
-  //       },
-  //       style: {},
-  //     };
-  //     return obj;
-  //   };
+//   document.createElement = document.captureStream = document.mozCaptureStream = function () {
+//     var obj = {
+//       getContext: function () {
+//         return obj;
+//       },
+//       play: function () {},
+//       pause: function () {},
+//       drawImage: function () {},
+//       toDataURL: function () {
+//         return "";
+//       },
+//       style: {},
+//     };
+//     return obj;
+//   };
 
-  //   that.HTMLVideoElement = function () {};
-  // }
+//   that.HTMLVideoElement = function () {};
+// }
 
-  if (typeof location === "undefined") {
-    /*global location:true */
-    that.location = {
-      protocol: "file:",
-      href: "",
-      hash: "",
-    };
-  }
+//   if (typeof location === "undefined") {
+//     /*global location:true */
+//     that.location = {
+//       protocol: "file:",
+//       href: "",
+//       hash: "",
+//     };
+//   }
 
-  if (typeof screen === "undefined") {
-    /*global screen:true */
-    that.screen = {
-      width: 0,
-      height: 0,
-    };
-  }
+//   if (typeof screen === "undefined") {
+//     /*global screen:true */
+//     that.screen = {
+//       width: 0,
+//       height: 0,
+//     };
+//   }
 
-  if (typeof URL === "undefined") {
-    /*global screen:true */
-    that.URL = {
-      createObjectURL: function () {
-        return "";
-      },
-      revokeObjectURL: function () {
-        return "";
-      },
-    };
-  }
+//   if (typeof URL === "undefined") {
+//     /*global screen:true */
+//     that.URL = {
+//       createObjectURL: function () {
+//         return "";
+//       },
+//       revokeObjectURL: function () {
+//         return "";
+//       },
+//     };
+//   }
 
-  /*global window:true */
-  // that.window = global;
-})(typeof global !== "undefined" ? global : null);
+//   /*global window:true */
+//   // that.window = global;
+// })(typeof global !== "undefined" ? global : null);
 
 // _____________________________
 // Cross-Browser-Declarations.js
